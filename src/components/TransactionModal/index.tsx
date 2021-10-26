89ba2c8b7e6560ac606c91c405b3911876c6ae48
Carlos André <31482507+carlos-dev@users.noreply.github.com>
2021-10-26 12:00:22 -0300

    definindo as cores dos botões

--- src/components/TransactionModal/index.tsx
+++ src/components/TransactionModal/index.tsx
@@ -73,6 +73,7 @@ export function TransactionModal({isOpen, onRequestClose}: TransactionModalProps
             type="button" 
             onClick={() => setType('deposit')}
             isActive={type === 'deposit'}
+            activeColor="green"
           >
             <img src={incomeImg} alt="Entrada" />
             <span>Entrada</span>
@@ -82,6 +83,7 @@ export function TransactionModal({isOpen, onRequestClose}: TransactionModalProps
             type="button" 
             onClick={() => setType('withdraw')}
             isActive={type === 'withdraw'}
+            activeColor="red"
           >
             <img src={outcomeImg} alt="Saída" />
             <span>Saída</span>
